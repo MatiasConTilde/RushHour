679f4bafa82416acf2b493294868b77a35b3ceac
MatiasSp <Chus@MacBook-Pro-de-Chus.local>
2015-01-10 18:48:07 +0100

    movimiento a saltos

--- Assets/movimiento.js
+++ Assets/movimiento.js
@@ -1,36 +1,31 @@
 ﻿#pragma strict
 
 var right : KeyCode;
-var left : KeyCode;
-
-function Start () {
-
-}
+var left  : KeyCode;
+var up  : KeyCode;
+var down  : KeyCode;
+var destination : int;
 
 function Update () {
 
-// si soy yo el cubo seleccionado ...
-// me muevo
-
-
-// esto funciona ...
-//Debug.Log(gameObject.name);
-
 if(Globales.carriedObject == gameObject){
-  // Debug.Log("Hola, soy " + Globales);
   
+  if(Input.GetKeyDown(left)){
+  	rigidbody.position.x= rigidbody.position.x-6;
+  }
+  if(Input.GetKeyDown(right)){
+  	rigidbody.position.x= rigidbody.position.x+6;
+  }
+  if(Input.GetKeyDown(up)){
+  	rigidbody.position.z= rigidbody.position.z+6;
+  }
+  if(Input.GetKeyDown(down)){
+  	rigidbody.position.z= rigidbody.position.z-6;
+  }
   
-  if (Input.GetKey (left)){
-
-	rigidbody.AddForce(-10, 0, 0);
-	}
-
-if (Input.GetKey (right)){
-	rigidbody.AddForce(10, 0, 0);
-	}
-
+  if (Mathf.Abs(rigidbody.position.x - destination)<0.01){
+  	rigidbody.position.x=destination;
+  }
+ }
+  
 }
-  
-  
-}
-
