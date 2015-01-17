fb2baba4755af110db2d5310d8f2413e937be7bb
cami14 <Camila@MacBook-Pro-de-Camila.local>
2015-01-17 19:02:21 +0100

    mover bien en cuadrados

--- Assets/movimiento.js
+++ Assets/movimiento.js
@@ -6,29 +6,63 @@ var up  : KeyCode;
 var down  : KeyCode;
 var destination : int;
 
-var inc : float = 6;
+var inc : float;
+var fuerza : float;
 
 function Update () {
-inc = 1;
+	inc = 6;
+	fuerza =1000;
 
-if(Globales.carriedObject == gameObject){
   
-  if(Input.GetKeyDown(left)){
-  	rigidbody.position.x= rigidbody.position.x-inc;
-  }
-  if(Input.GetKeyDown(right)){
-  	rigidbody.position.x= rigidbody.position.x+inc;
-  }
-  if(Input.GetKeyDown(up)){
-  	rigidbody.position.z= rigidbody.position.z+inc;
-  }
-  if(Input.GetKeyDown(down)){
-  	rigidbody.position.z= rigidbody.position.z-inc;
-  }
+	if (rigidbody.transform.localScale.x > rigidbody.transform.localScale.z){
+
+		Debug.Log("Estoy en " + rigidbody.position.x + " y voy a " + destination);
+		
+		if (Mathf.Abs(rigidbody.position.x - destination)<0.5){
+  			rigidbody.velocity.x = 0;
+  			rigidbody.position.x = destination;
+  		}
+		
+		if(Globales.carriedObject == gameObject){
+			if(rigidbody.velocity.x ==0){
+			
+			
+				if(Input.GetKeyDown(left)){
+					//rigidbody.position.x= rigidbody.position.x-inc;
+					rigidbody.AddForce(new Vector3(-fuerza, 0, 0));
+					destination = rigidbody.position.x - inc;
+				}
+				if(Input.GetKeyDown(right)){
+				    //rigidbody.position.x= rigidbody.position.x+inc;
+					rigidbody.AddForce(new Vector3(fuerza, 0, 0));
+					destination = rigidbody.position.x + inc;
+				 }
+			}
+		}
+	}
+	
+	if (rigidbody.transform.localScale.x < rigidbody.transform.localScale.z){
+		
+		if (Mathf.Abs(rigidbody.position.z - destination)<0.1){
+  			rigidbody.velocity.z = 0;
+  			rigidbody.position.z = destination;
+  		}
+		
+		if(Globales.carriedObject == gameObject){
+		  if(rigidbody.velocity.z ==0){
+				if(Input.GetKeyDown(up)){
+				  	//rigidbody.position.z= rigidbody.position.z+inc;
+				  	rigidbody.AddForce(new Vector3(0, 0, fuerza));
+				  	destination = rigidbody.position.z + inc;
+				}
+				if(Input.GetKeyDown(down)){
+				  	//rigidbody.position.z= rigidbody.position.z-inc;
+				  	rigidbody.AddForce(new Vector3(0, 0, -fuerza));
+				  	destination = rigidbody.position.z - inc;
+				}
+		    }  
+		}
+	}
   
-  if (Mathf.Abs(rigidbody.position.x - destination)<0.01){
-  	rigidbody.position.x=destination;
-  }
- }
   
 }
