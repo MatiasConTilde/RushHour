81268db569cde96920db244aeb17ef589137a394
cami14 <Camila@MacBook-Pro-de-Camila.local>
2015-01-17 17:26:13 +0100

    colores y medidas

--- Assets/movimiento.js
+++ Assets/movimiento.js
@@ -6,21 +6,24 @@ var up  : KeyCode;
 var down  : KeyCode;
 var destination : int;
 
+var inc : float = 6;
+
 function Update () {
+inc = 1;
 
 if(Globales.carriedObject == gameObject){
   
   if(Input.GetKeyDown(left)){
-  	rigidbody.position.x= rigidbody.position.x-6;
+  	rigidbody.position.x= rigidbody.position.x-inc;
   }
   if(Input.GetKeyDown(right)){
-  	rigidbody.position.x= rigidbody.position.x+6;
+  	rigidbody.position.x= rigidbody.position.x+inc;
   }
   if(Input.GetKeyDown(up)){
-  	rigidbody.position.z= rigidbody.position.z+6;
+  	rigidbody.position.z= rigidbody.position.z+inc;
   }
   if(Input.GetKeyDown(down)){
-  	rigidbody.position.z= rigidbody.position.z-6;
+  	rigidbody.position.z= rigidbody.position.z-inc;
   }
   
   if (Mathf.Abs(rigidbody.position.x - destination)<0.01){
